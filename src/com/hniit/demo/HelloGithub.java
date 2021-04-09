21ff3d14c5829e5dbe36326724a786d2171053f1
wenmaosi <473046664@qq.com>
2021-04-09 21:54:35 +0800

    test

--- src/com/hniit/demo/HelloGithub.java
+++ src/com/hniit/demo/HelloGithub.java
@@ -2,6 +2,6 @@ package com.hniit.demo;
 
 public class HelloGithub {
     public static void main(String[] args) {
-        System.out.println("Hello Github");
+        System.out.println("Hello");
     }
 }
